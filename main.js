58ddc9d8294792b91ef820c10325749d48be5e49
kost90 <kostdonc90@gmail.com>
2024-04-22 15:46:12 +0100

    edited error and warn functions - added msg to the params, edited export of getLogger

--- main.js
+++ main.js
@@ -2,4 +2,6 @@ const logger = require('./utils/logger')('main')
 
 logger.info('the script is running!')
 
-logger.error()
+logger.error('error: fail to running the script!')
+
+logger.warn('warn message!')
--- utils/logger.js
+++ utils/logger.js
@@ -2,20 +2,20 @@ function info(initParam, msg) {
   console.log(`${initParam}: ${msg}`);
 }
 
-function warn(initParam) {
-  console.error(`${initParam}: warn error`);
+function warn(initParam,msg) {
+  console.error(`${initParam}: ${msg}`);
 }
 
-function error(initParam) {
-  console.error(`${initParam}: error`);
+function error(initParam,msg) {
+  console.error(`${initParam}: ${msg}`);
 }
 
 function getLogger(initParam) {
   return {
     info: (msg) => info(initParam, msg),
-    warn: () => warn(initParam),
-    error: () => error(initParam),
+    warn: (msg) => warn(initParam,msg),
+    error: (msg) => error(initParam,msg),
   };
 }
 
-module.exports = (initParam) => getLogger(initParam);
+module.exports = getLogger;
